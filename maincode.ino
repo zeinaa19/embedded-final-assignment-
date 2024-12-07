1b62c86929bb4ebea6580132b378dcc85b92d116
Jumanaa19 <ja2201270@tkh.edu.eg>
2024-12-07 18:44:30 +0200

    Update maincode.ino

--- maincode.ino
+++ maincode.ino
@@ -1,3 +1,50 @@
 #include "Lcd.h"
 #include "adc.h"
-#include "motor,h"
+#include "motor.h"
+#include <util/delay.h>
+#include <stdlib.h>
+
+int main(void) {
+  buttoninit();
+  Adc_Init();
+  LCD_Init();
+  float temperature;
+  unsigned short adcsensor;
+  unsigned short upper=0;
+  unsigned short lower=0;
+  unsigned char buffer[6];
+  
+while (1) {
+  _delay_ms(200);
+
+  adcsensor=Adc_ReadChannel(1);
+  temperature=adcsensor*(5.0/1024.0)*100;
+
+  dtostrf(temperature,5,1,buffer);
+  if (((PINB>>PB4)&1)==0){
+    lower+=5;
+    _delay_ms(200);
+  if (((PINB>>PB5)&1)==0){
+    upper+=5;
+    _delay_ms(200);
+  if (((PIND>>PD0)&1)==0){
+    upper=0;
+    lower=0;
+  }
+LCD_Clear();
+LCD_Command(0x80);
+LCD_String("Temp:");
+LCD_String(buffer);
+LCD_String("C");
+
+LCD_Command(0xC0);
+LCD_String("L:");
+itoa(lower,buffer,10);
+LCD_String(buffer);
+LCD_String('U');
+itoa(upper,buffer,10);
+LCD_String(buffer);
+
+
+
+
